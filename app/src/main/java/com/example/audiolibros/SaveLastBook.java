c1ae328e5de56e6f41ec129a1f27fcb54638d4bf
Miguel <miibpa2@gmail.com>
2017-02-09 21:19:24 +0100

    Terminada primer tema, falta el refactor de Detalle Fragment

---
+++ app/src/main/java/com/example/audiolibros/SaveLastBook.java
@@ -0,0 +1,18 @@
+package com.example.audiolibros;
+
+/**
+ * Created by miguel on 5/2/17.
+ */
+
+public class SaveLastBook {
+    private final BooksRepository booksRepository;
+
+    public SaveLastBook(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public void execute(int id) {
+        booksRepository.saveLastBook(id);
+    }
+
+}
